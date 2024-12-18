ea9d0b11cb05877b46af5ac32127951a2c80a0f8
moshenetsb <bogdan.moshenets@outlook.com>
2024-12-18 12:59:06 +0100

    Zrobione menu logowania i logowanie za pomocą loginu i hasła

--- Projekt_PSiO/src/Logowanie/Logowanie_LoginHaslo.java
+++ Projekt_PSiO/src/Logowanie/Logowanie_LoginHaslo.java
@@ -4,8 +4,49 @@ public class Logowanie_LoginHaslo implements Logowanie {
 
 	@Override
 	public void logowanie() {
-		// TODO Auto-generated method stub
-		System.out.println("Logowanie odbywa się przez login i hasło");
+
+		// Odczytywanie loginu
+		System.out.print("Podaj login: ");
+		String login = MenuLogowanie.scanner.nextLine();
+		login = MenuLogowanie.scanner.nextLine();
+
+		// Sprawdzenie istnienia użytkowników z podanym loginem
+		int idKontaPracownika = MenuLogowanie.czyIstniejeLoginPracownika(login);
+		int idKontaKlienta = MenuLogowanie.czyIstniejeLoginKlienta(login);
+
+		// Jeśli błędny login
+		if (idKontaPracownika == -1 && idKontaKlienta == -1) {
+			System.out.println("Podany login nie istnieje!");
+			return;
+		}
+
+		// Odczytywanie hasła
+		System.out.print("Podaj hasło: ");
+		String haslo = MenuLogowanie.scanner.nextLine();
+		haslo = MenuLogowanie.scanner.nextLine();
+
+		// Logowanie klienta i sprawdzenie hasła
+		if (idKontaPracownika == -1) {
+
+			if (haslo == kontaKlientow[idKontaKlienta].getHaslo())
+				aktywnaOsoba = kontaKlientow[idKontaKlienta];
+			else {
+				System.out.println("Zostało podane błędne hasło!");
+				return;
+			}
+
+		}
+
+		// Logowanie pracownika i sprawdzenie hasła
+		else {
+			if (haslo == kontaPracownikow[idKontaPracownika].getHaslo())
+				aktywnaOsoba = kontaPracownikow[idKontaPracownika];
+			else {
+				System.out.println("Zostało podane błędne hasło!");
+				return;
+			}
+		}
+
 	}
 
 }
---
+++ Projekt_PSiO/src/Logowanie/MenuLogowanie.java
@@ -0,0 +1,116 @@
+package Logowanie;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MenuLogowanie {
+	
+	// Pola statyczne_______________________________________________
+	public static Scanner scanner = new Scanner(System.in);
+	public static boolean czyKoniecProgramu = false;
+	public static Logowanie preferowaneLogowanie = null;
+	// ________________________________________________________________
+	
+	public static void menuLogowania() {
+		System.out.print("____________Logowanie____________\n" 
+				+ "1) Logowanie\n"
+				+ "2) Modyfikuj preferowany sposób logowania\n" 
+				+ "3) Zakońć program\n" 
+				+ "_________________________________\n");
+
+		System.out.print("Wybierz opcje: ");
+		int wybor = scanner.nextInt();
+		
+		while (wybor < 1 || wybor > 3) {
+			System.out.println("Została wybrana nieistniejąca opcja!");
+			
+			System.out.print("Wybierz opcje: ");
+			wybor = scanner.nextInt();
+		}
+		
+		switch(wybor) {
+		case 1:
+			if (preferowaneLogowanie == null) {
+				System.out.println("Dla początku wybierz sposób logowania!");
+				wyborSposobuLogowania(false);
+			}
+			preferowaneLogowanie.logowanie();
+			break;
+			
+		case 2:
+			wyborSposobuLogowania(true);
+			break;
+			
+		case 3:
+			System.out.println("Program został zakończony");
+			czyKoniecProgramu = true;
+			break;
+		}
+		
+	}
+	
+	// Zmienna czyMoznaWroczyc pokazuje, czy jest dostępny punkt 3 menu
+	private static void wyborSposobuLogowania(boolean czyMoznaWroczyc) {
+		
+		int iloscOpcji = 2; // pokazuje iłość dostępnych opcji menu
+		
+		System.out.print("_____Wybierz_preferowany_sposób_logowania_____\n"
+				+ "1) Logowanie (login i hasło)\n"
+				+ "2) Logowanie (emain i hasło)\n");
+		
+		if (czyMoznaWroczyc) {
+			System.out.print("3) Wróć do menu logowania\n");
+			iloscOpcji = 3;
+		}
+			
+		System.out.print("_______________________________________________\n");
+		
+		System.out.print("Wybierz opcje: ");
+		int wybor = scanner.nextInt();
+		
+		while (wybor < 1 || wybor > iloscOpcji) {
+			System.out.println("Została wybrana nieistniejąca opcja!");
+			
+			System.out.print("Wybierz opcje: ");
+			wybor = scanner.nextInt();
+		}
+		
+		switch(wybor) {
+		case 1:
+			preferowaneLogowanie = new Logowanie_LoginHaslo();
+			break;
+		case 2:
+			preferowaneLogowanie = new Logowanie_EmailHaslo();
+			break;
+		case 3:
+			menuLogowania();
+			break;
+		}
+	}
+	
+	
+	// Zwraca -1, jeśli nie istnieje lub indeks osoby w tablicy, jeśli istnieje
+	public static int czyIstniejeLoginPracownika(String login) {
+		int czyIstnieje = -1;
+		
+		for (int i = 0; i < kontaPracownikow.size(); i++) {
+			if (login == kontaPracownikow[i].getLogin())
+				return i;
+		}
+		
+		return -1;
+	}
+	
+	// Zwraca -1, jeśli nie istnieje lub indeks osoby w tablicy, jeśli istnieje
+	public static int czyIstniejeLoginKlienta(String login) {
+		int czyIstnieje = -1;
+		
+		for (int i = 0; i < kontaKlientow.size(); i++) {
+			if (login == kontaKlientow[i].getLogin())
+				return i;
+		}
+		
+		return -1;
+	}
+		
+}
