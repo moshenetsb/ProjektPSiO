86428fb83beada23bd0fc6e78b8d3777c6b23f4a
StudentBJ <stawiamklocki@onet.pl>
2024-12-19 22:41:10 +0100

    Update Pracownik.java

--- Projekt_PSiO/src/osoba/Pracownik.java
+++ Projekt_PSiO/src/osoba/Pracownik.java
@@ -1,11 +1,32 @@
-package Osoba;
+package osoba;
 
-import Adres.Adres;
+import adres.Adres;
 
 public class Pracownik extends OsobaZarzadzajaca {
 
-	public Pracownik(String imieNazwisko,int wiek,Adres[] adres,String email, String pesel, float ocena, String pesel, float ocena) {
-		super(imieNazwisko,wiek,adres, email,pesel, ocena, pesel, ocena);
+
+
+
+	public Pracownik(String email, String haslo, String login, String nazwiskoImie, int wiek, Adres[] adres,
+			double saldoKonta, String pesel, float ocena) {
+		super(email, haslo, login, nazwiskoImie, wiek, adres, saldoKonta, pesel, ocena);
+	}
+
+	public void wyswietlProdukty() {
+
+		
+	}
+
+
+	public void wyswietlStanProduktu() {	
+	}
+
+	public void wyswietlStanKlienta() {
+		
+	}
+
+	public void wyswietlKlientow() {
+		
 	}
 	
 
